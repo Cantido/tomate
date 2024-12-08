515243fed26b74f97ecd8be3b30bf7d3c89ab7e4
Rosa Richter <cosmic.lady.rosa@gmail.com>
2024-12-08 12:48:54 -0700

    Actually make config path an arg

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ struct Args {
     #[clap(subcommand)]
     command: Command,
     /// Config file to use. [default: ${XDG_CONFIG_DIR}/tomate/config.toml]
+    #[arg(short, long)]
     config: Option<PathBuf>,
     #[command(flatten)]
     verbose: clap_verbosity_flag::Verbosity,
